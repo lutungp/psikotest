dc8c8484727bc55e389753e2bb8e15be1f9852eb
lutungp <lntngp19@gmail.com>
2021-02-07 14:07:19 +0700

    [UPDATE] hapus component sidebar

--- app/layout/components/DefaultSidebar.js
+++ app/layout/components/DefaultSidebar.js
@@ -9,7 +9,6 @@ import {
 import { SidebarMiddleNav } from './SidebarMiddleNav';
 
 import { SidebarTopA } from '../../routes/components/Sidebar/SidebarTopA'
-import { SidebarBottomA } from '../../routes/components/Sidebar/SidebarBottomA'
 import { LogoThemed } from '../../routes/components/LogoThemed/LogoThemed';
 
 export const DefaultSidebar = () => (
@@ -40,8 +39,6 @@ export const DefaultSidebar = () => (
                 { /* SIDEBAR: Menu */ }
                 <SidebarMiddleNav />
             </Sidebar.Section>
-
-            <SidebarBottomA />
         </Sidebar.MobileFluid>
         { /* END SIDEBAR: Only for Mobile */ }
     </Sidebar>
