601f9339461001e630fc732de2440f18da58858b
lutungp <lntngp19@gmail.com>
2021-02-07 14:19:29 +0700

    bersih2

--- app/layout/components/DefaultNavbar.js
+++ app/layout/components/DefaultNavbar.js
@@ -8,8 +8,6 @@ import {
     SidebarTrigger
 } from './../../components';
 
-import { NavbarActivityFeed } from './NavbarActivityFeed';
-import { NavbarMessages } from './NavbarMessages';
 import { NavbarUser } from './NavbarUser';
 import { LogoThemed } from './../../routes/components/LogoThemed/LogoThemed';
 
@@ -24,29 +22,8 @@ export const DefaultNavbar = () => (
                     <LogoThemed />
                 </Link>
             </NavItem>
-            <NavItem className="d-none d-md-block">
-                <span className="navbar-text">
-                    <Link to="/">
-                        <i className="fa fa-home"></i>
-                    </Link>
-                </span>
-                <span className="navbar-text px-2">
-                    <i className="fa fa-angle-right"></i>
-                </span>
-                <span className="navbar-text">
-                    <Link to="/">Start</Link>
-                </span>
-                <span className="navbar-text px-2">
-                    <i className="fa fa-angle-right"></i>
-                </span>
-                <span className="navbar-text">
-                    Page Link
-                </span>
-            </NavItem>
         </Nav>
         <Nav navbar className="ml-auto">
-            <NavbarActivityFeed />
-            <NavbarMessages className="ml-2" />
             <NavbarUser className="ml-2" />
         </Nav>
     </Navbar>
